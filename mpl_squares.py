c6093ccb22364938946644818bb33621a7986378
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-08-08 18:45:55 +0100

    squares graph completed

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 square = [1, 4, 9, 16, 25]
+plt.style.use("seaborn")
+
 fig, ax = plt.subplots()
-ax.plot(square)
+ax.plot(input_values, square, linewidth=3)
+
+# set chart title and label axes
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Squares of value", fontsize=14)
+
+# set size of tick labals
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
