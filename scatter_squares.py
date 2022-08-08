7d96f45ff584f6f7fe54b978c73e11a9495d841a
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-08-08 20:04:56 +0100

    scatter chart

---
+++ scatter_squares.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(2, 4, s=200)
+
+# set chart title and label axes
+ax.set_title("Square Number", fontsize=24)
+ax.set_xlabel
+
+plt.show()
