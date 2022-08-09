491f2d18fd3b02eaae3a3364b64f5677c3f7a02f
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-08-09 12:27:04 +0100

    scatter chart completed

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # set chart title and label axes
 ax.set_title("Square Number", fontsize=24)
-ax.set_xlabel
+ax.set_xlabel('Values', fontsize=14)
+ax.set_ylabel('Square of value', fontsize=14)
+
+# set size of tick label
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# set range for each axis
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
