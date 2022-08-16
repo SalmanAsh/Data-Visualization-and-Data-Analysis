b5aba2ec93886ce73e79cf9a0f1afc1a1a9f308f
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-08-16 14:18:34 +0100

    Temperatures graph

---
+++ sitka_highs_lows.py
@@ -0,0 +1,34 @@
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+filename = 'data/sitka_weather_2018_simple.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    # get dates, high and low temperatures from this file
+    dates, highs, lows = [], [], []
+    for row in reader:
+        current_date = datetime.strptime(row[2], "%Y-%m-%d")
+        high = int(row[5])
+        low = int(row[6])
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# plot the high and low temperatures
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# format plot
+ax.set_title('Daily high and low temperatures - 2018', fontsize=20)
+ax.set_xlabel('', fontsize=10)
+fig.autofmt_xdate()
+ax.set_ylabel('Temperature (F)', fontsize=13)
+ax.tick_params(axis='both', which='major', labelsize=13)
+
+plt.show()
